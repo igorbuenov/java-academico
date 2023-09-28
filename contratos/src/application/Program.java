dcfce86b9a1c85bcb6cbeef18a90645cf004fa6b
Igor Bueno <ib_ventura@hotmail.com>
2023-09-28 20:08:13 -0300

    Adicionando título do exercício proposto (contratos)

---
+++ contratos/src/application/Program.java
@@ -0,0 +1,19 @@
+// Exercício proposto
+/*Ler os dados de um trabalhadorcom N contratos (N fornecido pelo usuário).
+Depois, solicitar do usuário um mês e mostrar qual foi o salário do funcionário nesse mês, conforme o exemplo.*/
+
+
+
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
